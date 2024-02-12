2fcdc295daca2cd9bae7cc6337bf8773fe5b9d36
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-02-12 17:56:30 -0500

    landing page

--- src/app/api/open-ai/route.ts
+++ src/app/api/open-ai/route.ts
@@ -1,5 +1,19 @@
 import { NextResponse, NextRequest } from 'next/server';
+import OpenAIService from '@/services/open-ai.service';
+
+const OpenAI = new OpenAIService();
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ message: 'This is the Open AI route' });
+  const prompt = req.nextUrl.searchParams.get('prompt');
+
+  if (!prompt) {
+    return NextResponse.json({ message: 'Please provide a prompt' });
+  }
+
+  try {
+    const OpenAIResponse = await OpenAI.getChatResponse(prompt);
+    return NextResponse.json({ message: OpenAIResponse });
+  } catch (error) {
+    return NextResponse.json({ message: 'Error' });
+  }
 }
--- src/services/open-ai.service.ts
+++ src/services/open-ai.service.ts
@@ -9,6 +9,28 @@ class OpenAIService {
       apiKey: OPENAI_API_KEY,
     });
   }
+
+  async getChatResponse(prompt: string) {
+    try {
+      const response = await this.openAI.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content: 'You are a helpful assistant.',
+          },
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+      });
+
+      return response.choices[0].message.content;
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
 }
 
 export default OpenAIService;
